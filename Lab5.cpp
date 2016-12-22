8cbad82d63aa39c47e3bdf55ce6c270c0b138802
KozeevMaxim <maxim4244@gmail.com>
2016-12-22 13:33:59 +0300

    Update Lab5.cpp

--- Lab5.cpp
+++ Lab5.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
 using namespace std;
 
 double sum(float a, float b)
@@ -16,15 +19,31 @@ double mult(float a, float b)
     return a * b;
 }
 
-int main()
+int main(int argc, char *argv[])
 {
     int a, b;
-    cout << "a = ";
-    cin >> a;
-    cout << "b = ";
-    cin >> b;
+    if (argc < 3)
+    {
+        printf_s("Ошибка, параметров в командной строке не хватает для задания исходных данных. Для завершения нажмите любую клавишу\n");
+        system("pause");
+        return 1;
+    }
+    if (sscanf_s(argv[1], "%d", &a) < 1)
+    {
+        printf_s("Ошибка, неверный формат первого входного параметра. Для завершения нажмите любую клавишу\n");
+        system("pause");
+        return 1;
+    }
+    if (sscanf_s(argv[2], "%d", &b) < 1)
+    {
+        printf_s("Ошибка, неверный формат первого входного параметра. Для завершения нажмите любую клавишу\n");
+        system("pause");
+        return 1;
+    }
+
     cout << "a + b = " << sum(a, b) << endl;
     cout << "a - b = " << sub(a, b) << endl;
     cout << "a * b = " << mult(a, b) << endl;
+    system("pause");
     return 0;
 }
